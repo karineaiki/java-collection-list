93c0a13db82d99b3ec75e54c3e6fa44c143833f0
bastienapp <gitlab.bastienapp.m33f1@simplelogin.fr>
2024-08-31 17:35:19 +0200

    init

---
+++ Hero.java
@@ -0,0 +1,30 @@
+public class Hero {
+
+    // attributes
+    private String name;
+    private int age;
+
+    // constructors
+    public Hero(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    // getters
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    // setters
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
